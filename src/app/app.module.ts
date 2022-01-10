d3d1eee96a652c9e1bfd5efe5296146b6828776b
Blin-Jay <jay.huang@blinbuy.com>
2022-01-10 15:34:48 +0800

    pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { AboutComponent } from './about/about.component';
 import { ErrorComponent } from './error/error.component';
 import { MatButtonModule } from '@angular/material/button';
 import { SwiperComponent } from './component/swiper/swiper.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, AboutComponent, ErrorComponent, SwiperComponent],
@@ -17,6 +19,12 @@ import { SwiperComponent } from './component/swiper/swiper.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
